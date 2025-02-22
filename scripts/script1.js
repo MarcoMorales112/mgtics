63d00246423542212a2791366a470d06cc826c71
MarcoMorales112 <mark.colors112@gmail.com>
2025-02-22 13:25:51 -0800

    cambios en proyectos

---
+++ scripts/script1.js
@@ -0,0 +1,43 @@
+
+
+//String 
+
+var firsName = "Samantha";
+var lasName  = "Jimenez";
+
+firsName = "Fernanda";
+console.log(firsName);
+
+
+
+
+
+
+//numerical
+let myAge=99;
+let myHeiht=100;
+console.log(myAge,myHeiht)
+
+//Boolena (true/false)
+
+let isStuden =  true;
+
+
+let num1= 10;
+let num2= 10;
+
+
+console.log("El resultados es ="  +(num1+num2));
+console.log("El resultados es ="  +(num1-num2));
+console.log("El resultados es ="  +(num1*num2));
+console.log("El resultados es ="  +(num1/num2).toFixed(2));
+
+
+alert("Esto es un mensaje de alerta");
+
+
+
+document.write("El resultados es ="  +(num1+num2));
+
+
+
